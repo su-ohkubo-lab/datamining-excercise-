26233afa94ba7d0cd4a30472db7683ec40cdfc9b
Takato Nakamura <pptaka10gg@gmail.com>
2019-07-13 01:57:55 +0900

    [update] 3_classification/svm_training.py

--- 3_classification/svm_training.py
+++ 3_classification/svm_training.py
@@ -4,17 +4,17 @@ import numpy as np
 
 with open("iris_training.csv", 'r') as file:
     header = file.readline()
-    data = np.loadtxt(file, delimiter=',', usecols=(0,1,2,3,4))
+    data = np.loadtxt(file, delimiter=',', usecols=(0, 1, 2, 3, 4))
 
-inputs = data[:,0:2]
-labels = data[:,4]
+inputs = data[:, 0:2]
+labels = data[:, 4]
 
-type0 = inputs[labels==0]
-type1 = inputs[labels==1]
+type0 = inputs[labels == 0]
+type1 = inputs[labels == 1]
 
 training_inputs = np.r_[type0, type1]
-training_labels = np.r_[np.zeros(len(type0)),np.ones(len(type1))]
+training_labels = np.r_[np.zeros(len(type0)), np.ones(len(type1))]
 
 clf = svm.LinearSVC()
 clf.fit(training_inputs, training_labels)
-joblib.dump(clf, 'linear_svm.pkl') 
+joblib.dump(clf, 'linear_svm.pkl')
